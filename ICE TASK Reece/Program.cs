dd149269818486a8b09b636d46d99788261fe293
lab_services_student <lab_services_student@lab000000>
2023-04-12 11:25:24 +0000

    Add project files.

---
+++ ICE TASK Reece/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ICE_TASK_Reece
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
